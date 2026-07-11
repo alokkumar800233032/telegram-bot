ab0b291041f27ff7c1885143e36edbccbc9935ff
alokkumar800233032 <alokkumar8002330325@gmail.com>
2026-07-11 08:48:51 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+TOKEN = "YOUR_BOT_TOKEN"
+
+WELCOME_IMAGE = "PASTE_YOUR_FILE_ID_HERE"
+
+WELCOME_TEXT = """
+🎥 [WELCOME VIDEO / GIF]
+
+🌟 WELCOME 🌟
+
+💗 Hello..
+
+👤 Name : {first_name}
+🆔 Username : @{username}
+
+📎 Welcome to ❣️ AK GAMER CHAT ❣️
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+1️⃣ Respect Everyone 🤝
+2️⃣ 🚫 No Spam / No Abuse
+3️⃣ 🚫 No Links Without Permission
+4️⃣ 💚 Stay Active
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+🔗 Official Social Links
+
+📢 Channel : https://t.me/joing_ak_gamer_channel
+👥 Group : https://t.me/Ak_gamer_chat
+💬 WhatsApp : https://whatsapp.com/channel/0029VbCO0u0G8l5AKfymSx00
+▶️ YouTube : https://youtube.com/@AK_GAMER_900K
+📸 Instagram : https://instagram.com/ak_gamer_60k
+👑 Owner : https://t.me/ak_gamers
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+"""
